74ebec60e7b517a46905226f357af886e5dd5e1e
lathifa26 <92505141+lathifa26@users.noreply.github.com>
2022-11-21 18:44:15 +0530

    https://github.com/lathif26/Tic-Tac-Toe-Game.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [winner, setWinner] = useState('');
+  const [char, setChar] = useState('X');
+  const [matrix, setMatrix] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+  const getBGClass = (value) =>{
+    if(value === 'X') return 'yellow';
+    if(value === 'O') return 'orange';
+
+    return ''
+  };
+  const checkWinner = () =>{
+    //check row winner
+    if(matrix[0][0] && matrix[0][0]=== matrix[0][1] && matrix[0][1]=== matrix[0][2]){
+      setWinner(matrix[0][0] + 'is the winner');
+    }
+    if(matrix[1][0] && matrix[1][0]=== matrix[1][1] && matrix[1][1]=== matrix[1][2]){
+      setWinner(matrix[1][0] + 'is the winner');
+    }
+    if(matrix[2][0] && matrix[2][0]=== matrix[2][1] && matrix[2][1]=== matrix[2][2]){
+      setWinner(matrix[2][0] + 'is the winner');
+    }
+    //check col winner
+    if(matrix[0][0] && matrix[0][0]=== matrix[1][0] && matrix[1][0]=== matrix[2][0]){
+      setWinner(matrix[2][0] + 'is the winner');
+    }
+    if( matrix[0][1] && matrix[0][1]=== matrix[1][1] && matrix[1][1]=== matrix[2][1]){
+      setWinner(matrix[2][1] + 'is the winner');
+    }
+    if(matrix[0][2] && matrix[0][2]=== matrix[1][2] && matrix[1][2]=== matrix[2][2]){
+      setWinner(matrix[2][2] + 'is the winner');
+    }
+    //check diagnolly
+    if(matrix[0][0] && matrix[0][0]=== matrix[1][1] && matrix[1][1]=== matrix[2][2]){
+      setWinner(matrix[2][2] + 'is the winner');
+    }
+    if(matrix[0][2] && matrix[0][2]=== matrix[1][1] && matrix[1][1]=== matrix[2][0]){
+      setWinner(matrix[2][0] + '  is the winner');
+    }
+    if(count === 9){
+      setCount('The match has been drawn');
+    }
+  }
+  const handleClick = (r, c) => {
+    if(matrix[r][c]) return;
+    const tmpMatrix = [...matrix];
+    tmpMatrix[r][c] = char;
+    setMatrix(tmpMatrix);
+    setChar(char === 'X' ? 'O' : 'X');
+    setCount(count + 1);
+    checkWinner();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header alignCenter">Tic Tac Toe</div>
+      <div className="alignCenter board">
+        { !winner && <p>{char} turn now</p>}
+        <div className="gameBoard">
+          
+        { winner || matrix.map((row, rIndex) => (
+            <div className="row">
+              {row.map((cell, cIndex)=>(
+                  <div 
+                  onClick={()=> handleClick(rIndex, cIndex)}
+                   className={`cell alignCenter
+                   ${getBGClass(matrix[rIndex][cIndex])}`}>
+                    {matrix[rIndex][cIndex]}</div>
+              ))
+              }
+            </div>
+        ))}
+        </div>
+        <button onClick={()=>{
+          setWinner('');
+          setMatrix([
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
+          ]);
+        }}>Restart Game</button>
+      </div>
     </div>
   );
 }
